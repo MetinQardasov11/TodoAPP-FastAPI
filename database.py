10324d3b75dac2dd2a76caf47635ce96c0ecbe35
MetinQardasov11 <metin.qardasov2003@gmail.com>
2025-11-17 21:01:33 +0400

    Updated

--- database.py
+++ database.py
@@ -1,10 +1,11 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-POSTGRES_DATABASE_URL = "postgresql://postgres:11112003Mm@localhost/TodoApplication"
+# POSTGRES_DATABASE_URL = "postgresql://postgres:11112003Mm@localhost/TodoApplication"
 # MYSQL_DATABASE_URL = "mysql+pymysql://root:11112003mm@127.0.0.1:3306/TodoApplicationDB"
+SQLITE_DATABASE_URL = 'sqlite:///./todos.db'
 
-engine = create_engine(POSTGRES_DATABASE_URL)
+engine = create_engine(SQLITE_DATABASE_URL)
 # engine = create_engine(MYSQL_DATABASE_URL)
 
 SessionLocal = sessionmaker(autoflush=False, autocommit=False, bind=engine)
